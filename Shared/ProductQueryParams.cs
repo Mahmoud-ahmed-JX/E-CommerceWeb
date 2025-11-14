f3d108785748c8d861f32a6c0ad0e2c8b2907923
Mahmoud Ahmed <shahdelfares515@gmail.com>
2025-11-14 20:55:36 +0200

    Add files via upload

---
+++ Shared/ProductQueryParams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Enums;
+
+
+namespace Shared
+{
+    public class ProductQueryParams
+    {
+        private const int DeafultPageSize = 5;
+        private const int MaxPageSize = 10;
+        public int?TypeId {get;set;}
+         public int?BrandId { get; set; }
+        public ProductSortingOptions sort { get; set; }
+        public string? search { get; set; }
+        public int pageNumber { get; set; } = 1;
+        private int pagesize= DeafultPageSize;
+
+        public int PageSize
+        {
+            get { return pagesize; }
+            set { pagesize=  value>MaxPageSize?MaxPageSize:value; }
+        }
+
+
+
+
+
+
+
+    }
+}
